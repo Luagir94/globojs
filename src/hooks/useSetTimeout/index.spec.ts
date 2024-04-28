29e67276238be76764028e0ac3705161c345f487
Luciano Giraudi <lucianogiraudi@gmail.com>
2024-04-28 03:19:09 -0300

    [CHANGE} Optimized exports

--- src/hooks/useSetTimeout/index.spec.ts
+++ src/hooks/useSetTimeout/index.spec.ts
@@ -1,5 +1,6 @@
-import { renderHook, act } from '@testing-library/react'
+import { renderHook } from '@testing-library/react'
 import useSetTimeout from './'
+import { act } from 'react'
 
 describe('useSetTimeout', () => {
 	beforeEach(() => {
